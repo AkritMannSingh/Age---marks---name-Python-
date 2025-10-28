5f1ec3573a1521c9fd23a494dbf04615fb8706dd
Akrit Mann Singh <akritmannsingh@gmail.com>
2025-10-28 12:52:57 +0530

    Create program_collage.py

---
+++ program_collage.py
@@ -0,0 +1,10 @@
+name = input('Enter the name of the student:')
+age = int(input('Enter the age of the students'))
+marks = int (input('Enter the marks of the students:'))
+if(age > 18):
+if(marks > 65):
+print (name, 'is selected for collage')
+else:
+print(name, 'is not selected for collage')
+else:
+print (name, 'is too young for collage')
